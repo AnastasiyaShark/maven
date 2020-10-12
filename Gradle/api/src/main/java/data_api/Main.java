63309a20b53783dde284247b4fd417d1bd78322c
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2020-10-12 16:26:29 +0200

    3

---
+++ Gradle/api/src/main/java/data_api/Main.java
@@ -0,0 +1,13 @@
+package data_api;
+
+import data_db.MyEntity;
+import data_service.MyService;
+
+public class Main {
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
